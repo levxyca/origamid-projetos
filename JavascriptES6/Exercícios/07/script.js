ab8eb91f504fd49c9b905a88f9e374e754380b31
Leticia <leticiacaroline-123@hotmail.com>
2021-04-06 17:26:46 -0300

    add ex07

---
+++ JavascriptES6/Exercícios/07/script.js
@@ -0,0 +1,29 @@
+// nomeie 3 propriedades ou métodos de strings
+
+let nome = "Leticia";
+
+console.log(nome);
+console.log(nome.length);
+console.log(nome.charAt(0));
+console.log(nome.replace("ticia", "vxyca"));
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+/* 
+
+.lenght
+.replace
+.toString
+.toFixed
+.charAt
+
+*/
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+/* 
+
+ClipboardEvent
+
+*/
