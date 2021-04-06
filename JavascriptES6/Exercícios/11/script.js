36384abba31cf650a73ba62af21923d313d386c8
Leticia <leticiacaroline-123@hotmail.com>
2021-04-06 18:07:30 -0300

    add ex11, ex12, ex13

--- JavascriptES6/Exercícios/11/script.js
+++ JavascriptES6/Exercícios/11/script.js
@@ -1,13 +1,15 @@
 // Retorne o url da página atual utilizando o objeto window
-
-alert(location);
+const hrefPagina = window.location.href;
+console.log(hrefPagina);
 
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
 
 // Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
 
-
-// Retorne a largura da janela
-
-console.log(window.innerWidth);
+// Retorne a largura da janela 
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
