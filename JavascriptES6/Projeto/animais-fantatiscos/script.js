690c4f4483fa5f14354bd01161047be038e5723f
Leticia <leticiacaroline-123@hotmail.com>
2021-04-13 15:43:07 -0300

    Navegação por Tabs

--- JavascriptES6/Projeto/animais-fantatiscos/script.js
+++ JavascriptES6/Projeto/animais-fantatiscos/script.js
@@ -1,16 +1,19 @@
-// Quando o usuário clicar nos links internos do site,
-// adicione a classe ativo ao item clicado e remova dos
-// demais itens caso eles possuam a mesma. Previna
-// o comportamento padrão desses links
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
+if (tabMenu.length && tabContent) {
+  tabContent[0].classList.add("ativo");
 
-// Selecione todos os elementos do site começando a partir do body,
-// ao clique mostre exatamente quais elementos estão sendo clicados
+  function activeTab(index) {
+    tabContent.forEach((content) => {
+      content.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
 
-
-// Utilizando o código anterior, ao invés de mostrar no console,
-// remova o elemento que está sendo clicado, o método remove() remove um elemento
-
-
-// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
-
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
